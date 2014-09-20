ff268230d5d0ff0aeb6f52ba8bdac95164f68023
Ignacio Lis <ilismal@users.noreply.github.com>
2014-09-20 14:50:56 +0200

    Create plantaFeliz.ino

---
+++ plantaFeliz.ino
@@ -0,0 +1,76 @@
+//Librerias necesarias para la matriz de LEDs
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+//Matrix
+Adafruit_8x8matrix matriz = Adafruit_8x8matrix();
+//Variable para almacenar la lectura de humedad
+int humedad = 0;
+
+void setup() {
+  Serial.begin(57600);
+  matriz.begin(0x70); //Inicializacion de la matriz
+}
+//Dibujamos las caritas
+static const uint8_t PROGMEM
+  planta_contenta[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10100101,
+    B10011001,
+    B01000010,
+    B00111100 },
+  planta_triste[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10011001,
+    B10100101,
+    B01000010,
+    B00111100 },
+   planta_ahogada[] = 
+   {
+     B00111100,
+     B01000010,
+     B10100101,
+     B11001111,
+     B11101111,
+     B11111111,
+     B11111111,
+     B11111111
+   };
+
+void loop() {
+  humedad = analogRead(0);
+  Serial.print("Lectura del valor de humedad:");
+  Serial.println(humedad);
+  //Si la lectura de humedad esta por debajo de 300
+  //la planta necesita agua y esta triste
+  if (humedad < 300) {
+    matriz.clear();
+    matriz.drawBitmap(0, 0, planta_triste, 8, 8, LED_ON);
+    matriz.writeDisplay();
+    delay(500); 
+  }
+  //Si la lectura esta entre 300 y 700 la humedad
+  //es correcta y la planta esta contenta
+  else if ((humedad >= 300) && (humedad < 700)) {
+    matriz.clear();
+    matriz.drawBitmap(0, 0, planta_contenta, 8, 8, LED_ON);
+    matriz.writeDisplay();
+    delay(500);
+  }
+  //Lecturas superiores a 700 significan que la tierra esta
+  //demasiado humeda y la planta se ahoga
+  else {
+    matriz.clear();
+    matriz.drawBitmap(0, 0, planta_ahogada, 8, 8, LED_ON);
+    matriz.writeDisplay();
+    delay(500); 
+  }
+  delay(100);
+}
